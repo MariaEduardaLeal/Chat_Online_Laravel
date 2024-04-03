85044e9b80f055ccc9e7ac4ac163bf087524c881
MariaEduardaLeal <eduardaleal753@gmail.com>
2024-04-03 10:41:05 -0300

    Criando job para Enviar Mensagens

---
+++ app/Jobs/EnviarMensagem.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Events\RecebeMensagem;
+use App\Models\Messagem;
+use GuzzleHttp\Psr7\Message;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class EnviarMensagem implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Messagem $message)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        RecebeMensagem::dispatch([
+            'id' => $this->message->id,
+            'user_id' => $this->message->user_id,
+            'text' => $this->message->text,
+            'time' => $this->message->time,
+        ]);
+    }
+}
